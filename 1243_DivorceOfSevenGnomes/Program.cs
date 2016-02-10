41e99739c244f2757255bd3bcc3c688dd02bf247
Ilya Breev <ilyabreev@gmail.com>
2016-02-10 22:26:18 +0300

    problems 1243, 1893

---
+++ 1243_DivorceOfSevenGnomes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _1243_DivorceOfSevenGnomes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mod = 0;
+            int index = 1;
+            string input = Console.ReadLine();
+            int[] num = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                num[i] = int.Parse(input[i].ToString());
+            }
+
+            int test = num[0];
+            if (test / 7 > 0)
+            {
+                mod = test % 7;
+            }
+            else
+            {
+                test = num[0] * 10 + num[1];
+                mod = test % 7;
+                index++;
+            }
+
+            for (int i = index; i < num.Length; i++)
+            {
+                test = mod * 10 + num[i];
+                mod = test % 7;
+            }
+
+            Console.WriteLine(mod);
+        }
+    }
+}
---
+++ 1893_A380/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _1893_A380
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int row = 0;
+            char seat = 'X';
+            if (input.Length == 2)
+            {
+                row = Convert.ToInt32(input[0].ToString());
+                seat = input[1];
+            }
+            else
+            {
+                row = Convert.ToInt32(input.Substring(0, 2));
+                seat = input[2];
+            }
+
+            if (row == 1 || row == 2)
+            {
+                if (seat == 'A' || seat == 'D')
+                {
+                    Console.WriteLine("window");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("aisle");
+                    return;
+                }
+            }
+            else if (row >= 3 && row <= 20)
+            {
+                if (seat == 'A' || seat == 'F')
+                {
+                    Console.WriteLine("window");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("aisle");
+                    return;
+                }
+            }
+            else
+            {
+                if (seat == 'A' || seat == 'K')
+                {
+                    Console.WriteLine("window");
+                    return;
+                }
+                else if (seat == 'C' || seat == 'D' || seat == 'G' || seat == 'H')
+                {
+                    Console.WriteLine("aisle");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("neither");
+                    return;
+                }
+            }
+        }
+    }
+}
