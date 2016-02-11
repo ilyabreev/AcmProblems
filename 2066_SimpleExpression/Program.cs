70880b21dda2e98f995862c1e5d8838ebf64b948
Ilya Breev <ilyabreev@gmail.com>
2016-02-11 13:36:23 +0300

    problem 2066, updated README

---
+++ 2066_SimpleExpression/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2066_SimpleExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            if (b == 0 || b == 1)
+            {
+                Console.WriteLine(a - b - c);
+                return;
+            }
+
+            Console.WriteLine(a - b * c);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # AcmProblems
 
 Solutions to some of the ACM ICPC problems.
+
+Every problem has a number in the name of directory. So you can easily find problem description in the [task archive][http://acm.timus.ru/problemset.aspx?space=1&page=all]
