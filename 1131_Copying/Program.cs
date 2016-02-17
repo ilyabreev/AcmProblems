2734f4fd48d377b12738d876a68139ee29007490
Ilya Breev <ilyabreev@gmail.com>
2016-02-17 16:16:35 +0300

    problems 1131, 1457

---
+++ 1131_Copying/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _1131_Copying
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            long n = long.Parse(input[0]);
+            long k = long.Parse(input[1]);
+
+            long t = 0;
+
+            n -= 1;
+            if (n == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            long l = 1 << 0;
+            for (int i = 0; l <= k; i++)
+            {
+                if (n >= l)
+                {
+                    n -= l;
+                    t += 1;
+                    l = l << 1;
+                }
+                else if (n == 0)
+                {
+                    Console.WriteLine(t);
+                    return;
+                }
+                else
+                {
+                    t += 1;
+                    Console.WriteLine(t);
+                    return;
+                }
+            }
+
+            t += n / k;
+            if (n % k > 0)
+            {
+                t += 1;
+            }
+
+            Console.WriteLine(t);
+        }
+    }
+}
---
+++ 1457_ExpectedValue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1457_ExpectedValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long s = 0;
+            string[] input = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                s += int.Parse(input[i]);
+            }
+
+            Console.WriteLine("{0:0.000000}", s / (double)n);
+        }
+    }
+}
